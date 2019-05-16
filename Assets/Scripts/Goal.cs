1770bcf932ce78691e7cc3be85c731e73c279748
Ben Yang <benyang@Bens-MacBook-Pro-2.local>
2019-05-16 09:07:45 -0400

    fade to black

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -9,11 +9,16 @@ public class Goal : MonoBehaviour {
 	public float enemyCount;
 	public Sprite activeSprite;
 	public Sprite inactiveSprite;
+	public SpriteRenderer black;
 
 	SpriteRenderer sr;
+	bool won;
 
 	void Start () {
 		sr = GetComponent<SpriteRenderer> ();
+		black = GameObject.FindWithTag ("black").GetComponent<SpriteRenderer> ();
+		black.color = Color.white;
+		StartCoroutine (FadeOutBlack (0.15f));
 	}
 	
 	void Update () {
@@ -27,12 +32,25 @@ public class Goal : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
-		if (col.CompareTag("player") && active) {
-			LevelClear ();
+		if (col.CompareTag("player") && active && !won) {
+			won = true;
+			StartCoroutine (NextLevel (0.7f));
 		}
 	}
 
-	void LevelClear() {
+	IEnumerator NextLevel(float duration) {
+		while (black.color.a < 1f) {
+			black.color = new Color (black.color.r, black.color.g, black.color.b, Mathf.Clamp01 (black.color.a + Time.deltaTime / duration));
+			yield return new WaitForSeconds (0.02f);
+		}
+
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+	IEnumerator FadeOutBlack(float duration) {
+		while (black.color.a > 0f) {
+			black.color = new Color (black.color.r, black.color.g, black.color.b, Mathf.Clamp01 (black.color.a - Time.deltaTime / duration));
+			yield return new WaitForSeconds (0.02f);
+		}
 	}
 }
