6ecf25e05c23a492bbf72b185856deae1b05eabe
Ben Yang <benyang@Bens-MacBook-Pro-2.local>
2019-05-19 23:13:08 -0400

    move script, fix black

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    
+    public GameObject player;
+
+    // Use this for initialization
+
+
+    void Awake()
+    {
+        
+        player = GameObject.FindGameObjectWithTag("player");
+        transform.parent = null;
+        transform.position = Vector3.zero;
+
+    }
+	void Start () {
+        transform.position= player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/PhysicalButton.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalButton : MonoBehaviour {
+
+    
+    public bool canRevert=true;
+    public bool animationRevert = true;
+
+    public GameObject[] objectToActives;
+    public GameObject[] objectToDisactives;
+
+    
+    public Vector3 offset;
+    private Vector3 targetPosition;
+    private Vector3 originalPosition;
+
+
+    public ClickState state;
+    [HideInInspector]
+    public enum ClickState
+    {
+        NoClick,
+        BeingClick,
+        IsClick,
+        BeingUp,
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        originalPosition = transform.position;
+        targetPosition = transform.position + offset;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (state)
+        {
+            case ClickState.NoClick:
+                break;
+            case ClickState.BeingClick:
+                transform.position = Vector3.Lerp(transform.position, targetPosition, 0.6f);
+                if ((transform.position - targetPosition).magnitude<=0.02f)
+                {
+                    state = ClickState.IsClick;
+                }
+                break;
+            case ClickState.IsClick:
+                break;
+            case ClickState.BeingUp:
+                transform.position = Vector3.Lerp(transform.position, originalPosition, 0.2f);
+                if (transform.position == originalPosition)
+                {
+                    state = ClickState.NoClick;
+                }
+                break;
+            default:
+                break;
+        }
+
+        
+           
+        
+	}
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        state = ClickState.BeingClick;
+
+        if (!col.CompareTag("floor"))
+        {
+            foreach (var objectToActive in objectToActives)
+            {
+                objectToActive.SetActive(true);
+            }
+
+            foreach (var objectToDisable in objectToDisactives)
+            {
+                objectToDisable.SetActive(false);
+            }
+           
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (!col.CompareTag("floor"))
+        {
+            if (animationRevert)
+            {
+                state = ClickState.BeingUp;
+            }
+            
+
+            if (canRevert)
+            {
+                foreach (var objectToActive in objectToActives)
+                {
+                    objectToActive.SetActive(false);
+                }
+
+                foreach (var objectToDisable in objectToDisactives)
+                {
+                    objectToDisable.SetActive(true);
+                }
+            }
+        }
+    }
+}
