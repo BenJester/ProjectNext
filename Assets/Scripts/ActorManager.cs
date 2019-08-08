b9135c0c9906171f1fa0638a3d307e9986df5540
yzitao <yzitao@foxmail.com>
2019-08-08 10:45:34 +0800

    no message

--- Assets/Scripts/ActorManager.cs
+++ Assets/Scripts/ActorManager.cs
@@ -5,54 +5,60 @@ using System;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ActorManager : MonoBehaviour {
+public class ActorManager : MonoBehaviour
+{
 
     private SpriteRenderer black;
-	Text text;
-	//private static ActorManager _instance;
-	//public static ActorManager Instance { 
-	//	get { 	
-	//		return _instance; 
-	//	}
-	//}
+    Text text;
+    //private static ActorManager _instance;
+    //public static ActorManager Instance { 
+    //	get { 	
+    //		return _instance; 
+    //	}
+    //}
 
-	//public static List<GameObject> enemies;
-	//public static List<GameObject> obj;
-	//public static List<GameObject> bullets;
+    //public static List<GameObject> enemies;
+    //public static List<GameObject> obj;
+    //public static List<GameObject> bullets;
 
-	void Awake () {
+    void Awake()
+    {
         black = GameObject.FindGameObjectWithTag("black").GetComponent<SpriteRenderer>();
-		text = GameObject.FindWithTag("Respawn").GetComponent<Text>();
-		//if (ActorManager._instance == null) {
-		//	ActorManager._instance = this;
-		//	_instance.Init ();
-		//} else {
-		//	if (ActorManager._instance != this) {
-		//		throw new InvalidOperationException ("Cannot have two instances of a Singleton");
-		//	}
-		//}
-	}
+        text = GameObject.FindWithTag("Respawn").GetComponent<Text>();
+        //if (ActorManager._instance == null) {
+        //	ActorManager._instance = this;
+        //	_instance.Init ();
+        //} else {
+        //	if (ActorManager._instance != this) {
+        //		throw new InvalidOperationException ("Cannot have two instances of a Singleton");
+        //	}
+        //}
+    }
 
-	private void Init () {
-		//enemies = new List<GameObject> ();
-		//obj = new List<GameObject> ();
-		//bullets = new List<GameObject> ();
-	}
+    private void Init()
+    {
+        //enemies = new List<GameObject> ();
+        //obj = new List<GameObject> ();
+        //bullets = new List<GameObject> ();
+    }
 
 
-	void Start () {
+    void Start()
+    {
         black.color = Color.white;
 
-        StartCoroutine(FadeOutBlack(0.5f));
-		StartCoroutine(FadeInText(0.1f,0.1f,0.2f));
+        StartCoroutine(FadeOutBlack(1f));
+        StartCoroutine(FadeInText(0.1f, 1f, 0.2f));
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyUp (KeyCode.Escape)) {
-			SceneManager.LoadScene (19);
-		}
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(24);
+        }
+    }
 
 
     IEnumerator FadeOutBlack(float duration)
@@ -60,22 +66,22 @@ public class ActorManager : MonoBehaviour {
         while (black.color.a > 0f)
         {
             black.color = new Color(black.color.r, black.color.g, black.color.b, Mathf.Clamp01(black.color.a - Time.deltaTime / duration));
-			yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds(0.05f);
         }
     }
-	IEnumerator FadeInText(float duration1, float duration2, float duration3)
-	{
-		while (text.color.a < 1f)
-		{
-			text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Clamp01(text.color.a + Time.deltaTime / duration1));
-			yield return new WaitForSeconds(0.02f);
-		}
-		yield return new WaitForSeconds(duration2);
+    IEnumerator FadeInText(float duration1, float duration2, float duration3)
+    {
+        while (text.color.a < 1f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Clamp01(text.color.a + Time.deltaTime / duration1));
+            yield return new WaitForSeconds(0.02f);
+        }
+        yield return new WaitForSeconds(duration2);
 
-		while (text.color.a > 0f)
-		{
-			text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Clamp01(text.color.a - Time.deltaTime / duration3));
-			yield return new WaitForSeconds(0.02f);
-		}
-	}
+        while (text.color.a > 0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Clamp01(text.color.a - Time.deltaTime / duration3));
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
 }
