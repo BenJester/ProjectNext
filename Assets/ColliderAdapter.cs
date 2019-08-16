49a71965b2942d133198f9d3ee50de48f8d12497
yzitao <yzitao@foxmail.com>
2019-08-16 17:22:55 +0800

    no message

---
+++ Assets/ColliderAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderAdapter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+		GetComponent<BoxCollider2D>().size=new Vector3(GetComponent<SpriteRenderer>().bounds.size.x,GetComponent<SpriteRenderer>().bounds.size.y,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
