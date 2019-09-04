06900c47ea1d1414dbb9a331990afab4c83a377b
yzitao <yzitao@foxmail.com>
2019-09-04 19:21:51 +0800

    加入不完善的影冲和smash

--- Assets/Scripts/Skills/Dash.cs
+++ Assets/Scripts/Skills/Dash.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Dash : Skill {
 
+	public bool isShadowDash = false;
 	public float DashSpeed;
 	public float DashDuration;
 	public float pauseDuration;
@@ -11,57 +12,60 @@ public class Dash : Skill {
 	public int maxCharge;
 	int charge;
 
-	public override void Init()
-	{
+	public override void Init () {
 		charge = maxCharge;
 	}
 
-	public override bool Check()
-	{
+	public override bool Check () {
 		return charge > 0;
 	}
 
-	void Update() {
-		if (playerControl.isTouchingGround) 
-		{
+	void Update () {
+		if (playerControl.isTouchingGround) {
 			charge = maxCharge;
 		}
 	}
 
-	public override void Do()
-	{
-		if (!active || !playerControl.canMove || !Check())
+	public override void Do () {
+		if (!active || !playerControl.canMove || !Check ())
 			return;
-		
+
 		playerControl.canMove = false;
 
 		StartCoroutine (DoDash ());
 	}
 
-	IEnumerator DoDash()
-	{
-		
+	IEnumerator DoDash () {
+
+		if (isShadowDash) {
+			player.GetComponent<SpriteRenderer> ().color = Color.black;
+			player.GetComponent<Collider2D> ().enabled = false;
+		}
+
 		float curr = 0f;
-//		while (curr < pauseDuration) 
-//		{
-//			playerBody.velocity = Vector2.zero;
-//			curr += Time.deltaTime;
-//			yield return new WaitForEndOfFrame ();
-//		}
-//		curr = 0f;
-//		float h = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
-//		float v = (Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0);
-//		Vector2 dir = new Vector2(h, v).normalized;
+		//		while (curr < pauseDuration) 
+		//		{
+		//			playerBody.velocity = Vector2.zero;
+		//			curr += Time.deltaTime;
+		//			yield return new WaitForEndOfFrame ();
+		//		}
+		//		curr = 0f;
+		//		float h = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
+		//		float v = (Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0);
+		//		Vector2 dir = new Vector2(h, v).normalized;
 		Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		Vector2 dir =  (mouseWorldPos - (Vector2) player.transform.position).normalized;
+		Vector2 dir = (mouseWorldPos - (Vector2) player.transform.position).normalized;
 		playerBody.velocity = dir * DashSpeed;
-		while (curr < DashDuration) 
-		{
+		while (curr < DashDuration) {
 			curr += Time.deltaTime;
 			yield return new WaitForEndOfFrame ();
 		}
 		charge -= 1;
 		playerBody.velocity = dir * playerControl.speed;
+		if (isShadowDash) {
+			player.GetComponent<Collider2D> ().enabled = true;
+			player.GetComponent<SpriteRenderer> ().color = Color.white;
+		}
 		playerControl.canMove = true;
 	}
 }
---
+++ Assets/Smash.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smash : Skill {
+
+	// Use this for initialization
+
+	public float smashSpeed=10f;
+	public float smashTransitionTime=0.2f;
+
+	public int maxCharge;
+	int charge;
+
+	public override void Init()
+	{
+		charge = maxCharge;
+	}
+
+	public override bool Check()
+	{
+		return charge > 0;
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerControl.isTouchingGround) 
+		{
+			charge = maxCharge;
+			StopCoroutine(DoSmash());
+
+		}
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			Do();
+		}
+		
+	}
+
+	public override void Do()
+	{
+		if (!active || !playerControl.canMove || !Check())
+			return;
+		
+		playerControl.canMove = false;
+
+		StartCoroutine (DoSmash ());
+	}
+
+	IEnumerator DoSmash()
+	{
+		Debug.Log("Smash!");
+		playerBody.velocity=Vector2.zero;
+		playerBody.gravityScale=0f;
+		yield return new WaitForSeconds(smashTransitionTime);
+		playerBody.gravityScale=200f;
+		playerBody.velocity = Vector2.down *smashSpeed;
+		playerControl.canMove = true;
+	}
+}
