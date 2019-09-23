861fad6759aaa5c4d153af24a5591201ba8ac1fc
BenJester <jichu.yang1998@gmail.com>
2019-09-22 23:52:41 -0400

    命中物体抖动镜头

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class Bullet : MonoBehaviour {
 
@@ -150,51 +151,50 @@ public class Bullet : MonoBehaviour {
 			playerControl.swap.col = col;
 			playerControl.swap.Do ();
 			playerControl.doubleSwap = true;
+            CameraShaker.Instance.ShakeOnce(15, 1f, 0.02f, 0.05f);
 
-		
-			
 
-			//			Rigidbody2D thingBody = col.gameObject.GetComponent<Rigidbody2D> ();
-			//			Thing thing = col.gameObject.GetComponent<Thing> ();
-			//			Vector3 pos = player.transform.position;
-			//			Vector3 thingPos = col.transform.position;
-			//			float playerRadiusY = player.GetComponent<BoxCollider2D> ().size.y / 2f;
-			//			float heightDiff = (col.GetComponent<BoxCollider2D> ().size.y * col.transform.localScale.y - playerRadiusY * 2f) / 2f;
-			//
-			//			if (thing.leftX < player.transform.position.x && thing.rightX > player.transform.position.x && thing.lowerY > player.transform.position.y && thing.lowerY < player.transform.position.y + playerRadiusY + 10f) {
-			//				if () {
-			//				Vector3 temp = col.gameObject.transform.position;
-			//				col.gameObject.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - playerRadiusY + (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
-			//				player.transform.position = new Vector3 (temp.x, col.gameObject.transform.position.y + playerRadiusY + (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
-			//
-			//				
-			//				
-			//					//	} else {
-			//					Vector3 temp = player.transform.position;
-			//					player.transform.position = new Vector3 (col.gameObject.transform.position.x, col.gameObject.transform.position.y + player.GetComponent<BoxCollider2D> ().size.y / 2f - (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
-			//					col.gameObject.transform.position = new Vector3 (temp.x, player.gameObject.transform.position.y + player.GetComponent<BoxCollider2D> ().size.y / 2f + (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
-			//				}
-			//
-			//			} else {
-			//				Vector3 tempPos = new Vector3 (pos.x, pos.y + heightDiff, pos.z);
-			// 				GameObject par1 = Instantiate(dashParticle,player.transform.position+smokeOffset,Quaternion.identity);
-			// 				GameObject par2 = Instantiate(dashParticle,thingPos+smokeOffset,Quaternion.identity);
-			// 				Destroy(par1,1f);
-			// 				Destroy(par2,1f);
-			// //				player.transform.position = new Vector3 (thingPos.x, thingPos.y - heightDiff, thingPos.z);
-			// //				col.gameObject.transform.position = tempPos;
-			// //				//交换的瞬间
-			// //				
-			// 				PostEffectManager.instance.Blink(0.03f);
-			//			}
-			//
-			//
-			//
-			//
-			//			Vector2 tempV = playerBody.velocity;
-			//			playerBody.velocity = thingBody.velocity;
-			//			thingBody.velocity = tempV;
-			Deactivate ();
+            //			Rigidbody2D thingBody = col.gameObject.GetComponent<Rigidbody2D> ();
+            //			Thing thing = col.gameObject.GetComponent<Thing> ();
+            //			Vector3 pos = player.transform.position;
+            //			Vector3 thingPos = col.transform.position;
+            //			float playerRadiusY = player.GetComponent<BoxCollider2D> ().size.y / 2f;
+            //			float heightDiff = (col.GetComponent<BoxCollider2D> ().size.y * col.transform.localScale.y - playerRadiusY * 2f) / 2f;
+            //
+            //			if (thing.leftX < player.transform.position.x && thing.rightX > player.transform.position.x && thing.lowerY > player.transform.position.y && thing.lowerY < player.transform.position.y + playerRadiusY + 10f) {
+            //				if () {
+            //				Vector3 temp = col.gameObject.transform.position;
+            //				col.gameObject.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - playerRadiusY + (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
+            //				player.transform.position = new Vector3 (temp.x, col.gameObject.transform.position.y + playerRadiusY + (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
+            //
+            //				
+            //				
+            //					//	} else {
+            //					Vector3 temp = player.transform.position;
+            //					player.transform.position = new Vector3 (col.gameObject.transform.position.x, col.gameObject.transform.position.y + player.GetComponent<BoxCollider2D> ().size.y / 2f - (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
+            //					col.gameObject.transform.position = new Vector3 (temp.x, player.gameObject.transform.position.y + player.GetComponent<BoxCollider2D> ().size.y / 2f + (thing.upperY - thing.lowerY) / 2f, player.transform.position.z);
+            //				}
+            //
+            //			} else {
+            //				Vector3 tempPos = new Vector3 (pos.x, pos.y + heightDiff, pos.z);
+            // 				GameObject par1 = Instantiate(dashParticle,player.transform.position+smokeOffset,Quaternion.identity);
+            // 				GameObject par2 = Instantiate(dashParticle,thingPos+smokeOffset,Quaternion.identity);
+            // 				Destroy(par1,1f);
+            // 				Destroy(par2,1f);
+            // //				player.transform.position = new Vector3 (thingPos.x, thingPos.y - heightDiff, thingPos.z);
+            // //				col.gameObject.transform.position = tempPos;
+            // //				//交换的瞬间
+            // //				
+            // 				PostEffectManager.instance.Blink(0.03f);
+            //			}
+            //
+            //
+            //
+            //
+            //			Vector2 tempV = playerBody.velocity;
+            //			playerBody.velocity = thingBody.velocity;
+            //			thingBody.velocity = tempV;
+            Deactivate();
 
 		} else if (col.CompareTag ("floor") || col.CompareTag("shield")) {
 			Deactivate ();
