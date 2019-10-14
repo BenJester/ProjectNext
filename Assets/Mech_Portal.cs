dbe75e7fb6f923cb68064669e94e58539b846df7
jeffyxie <jeffyxie@tencent.com>
2019-10-14 16:45:05 +0800

    添加机制：传送门、弹簧

---
+++ Assets/Mech_Portal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mech_Portal : MonoBehaviour {
+
+	public Mech_Portal target;
+    public Vector2 dir;
+    private Vector2 targetPos;
+    [HideInInspector]public bool canPort = true;
+	void Start () {
+        canPort = true;
+        targetPos = target.transform.position;
+	}
+	
+	private void OnTriggerEnter2D(Collider2D col) {
+        if (!target || !canPort)
+            return;
+        if (target.dir != Vector2.zero)
+        {
+            Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+            float speed = rb.velocity.magnitude;
+            rb.velocity = target.dir * speed;
+        }
+        col.transform.position = targetPos;
+
+        SetNoPort();
+        Invoke("SetCanPort", 0.05f);
+	}
+    private void SetCanPort()
+    {
+        target.canPort = true;
+        canPort = true;
+    }
+    private void SetNoPort()
+    {
+        target.canPort = false;
+        canPort = false;
+    }
+}
