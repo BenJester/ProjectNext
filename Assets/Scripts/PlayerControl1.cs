951d92602511d78b41b23e572cb43af6e871f78c
BenJester <jichu.yang1998@gmail.com>
2019-10-18 01:57:36 -0400

    line locked on

--- Assets/Scripts/PlayerControl1.cs
+++ Assets/Scripts/PlayerControl1.cs
@@ -172,7 +172,9 @@ public class PlayerControl1 : PlayerControl {
 		dash = GetComponent<Dash> ();
 	}
 
-	void Awake () {
+    public LineRenderer lockedOnObjectLine;
+
+    void Awake () {
 
 		originalScale = transform.localScale;
 		startDeltaTime = Time.fixedDeltaTime;
@@ -188,8 +190,10 @@ public class PlayerControl1 : PlayerControl {
 
 	void Start () {
 
+        lockedOnObjectLine.startWidth = 1f;
+        lockedOnObjectLine.positionCount = 2;
 
-		blackSr = GameObject.FindWithTag ("black").GetComponent<SpriteRenderer> ();
+        blackSr = GameObject.FindWithTag ("black").GetComponent<SpriteRenderer> ();
 		bulletSpeed = minBulletSpeed;
 
 		InitSkills ();
@@ -502,9 +506,24 @@ public class PlayerControl1 : PlayerControl {
 		// 记号圆圈
 		if (closestObjectToCursor != null) {
 			marker.transform.position = new Vector3 (closestObjectToCursor.transform.position.x, closestObjectToCursor.transform.position.y, -1f);
-		} else {
+            lockedOnObjectLine.SetPosition(0, transform.position);
+            lockedOnObjectLine.SetPosition(1, closestObjectToCursor.transform.position);
+            RaycastHit2D hit = Physics2D.Raycast (transform.position, (closestObjectToCursor.transform.position - transform.position).normalized, shootDistance, 1 << 10 | 1 << 12 | 1 << 8);
+            lockedOnObjectLine.startWidth = 1f;
+            if (hit.collider == closestObjectToCursor.GetComponent<BoxCollider2D>())
+            {
+                swap.col = closestObjectToCursor.GetComponent<BoxCollider2D>();
+                lockedOnObjectLine.startWidth = 5f;
+            }
+            if (swap.col && Vector3.Distance(swap.col.transform.position, transform.position) > shootDistance)
+            {
+                lockedOnObjectLine.startWidth = 0f;
+            }
+        } else {
 			marker.transform.position = new Vector3 (-10000f, 0f, 0f);
-		}
+            lockedOnObjectLine.SetPosition(0,Vector3.zero);
+            lockedOnObjectLine.SetPosition(1,Vector3.zero);
+        }
 		if (swap.col != null && doubleSwap && !swap.col.GetComponent<Thing> ().dead) {
 			targetMarker.transform.position = new Vector3 (swap.col.transform.position.x, swap.col.gameObject.transform.position.y, -1f);
 		} else {
@@ -621,11 +640,19 @@ public class PlayerControl1 : PlayerControl {
 	//狙击枪，点击直线瞬间交换；
 	void HandleLaserChange () {
 		Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-
-		RaycastHit2D hit = Physics2D.Raycast (transform.position, (closestObjectToCursor.transform.position - transform.position).normalized, shootDistance, ~(1 << 9));
-
-		if (hit.collider == closestObjectToCursor.GetComponent<BoxCollider2D> ()) {
+        if (!closestObjectToCursor) return;
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, (closestObjectToCursor.transform.position - transform.position).normalized, shootDistance, 1 << 10 | 1 << 12 | 1 << 8);
+        if (!hit) return;
+        lockedOnObjectLine.startWidth = 1f;
+        if (hit.collider == closestObjectToCursor.GetComponent<BoxCollider2D> ()) {
 			swap.col = closestObjectToCursor.GetComponent<BoxCollider2D> ();
+            lockedOnObjectLine.startWidth = 3f;
+            if (Vector3.Distance(swap.col.transform.position, transform.position) > shootDistance)
+            {
+                lockedOnObjectLine.startWidth = 0f;
+                return;
+            }
+            
 			swap.Do ();
 		}
 
