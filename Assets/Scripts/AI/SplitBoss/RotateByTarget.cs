8e8de11cf78e70b469d41157b972af1020aee4da
kaleidosgu <gjcaoyi@163.com>
2019-10-21 21:05:54 +0800

    boss相关添加

---
+++ Assets/Scripts/AI/SplitBoss/RotateByTarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateByTarget : MonoBehaviour
+{
+    public Transform AroundTarget;//围绕的目标
+    public float AngularSpeed;//角速度
+    public float AroundRadius;//半径
+
+    public float DiffAngle;
+
+    private float m_fAngled;
+
+    private SplitBossPartCollision m_collision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_collision = GetComponent<SplitBossPartCollision>();
+    }
+
+    public void StartRotate(Transform _target)
+    {
+        AroundTarget = _target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(AroundTarget != null)
+        {
+            if(m_collision.IsCollision() == false)
+            {
+                m_fAngled += (AngularSpeed * Time.deltaTime) % 360;//累加已经转过的角度
+                float posX = AroundRadius * Mathf.Sin((m_fAngled + DiffAngle) * Mathf.Deg2Rad);//计算x位置
+                float posZ = AroundRadius * Mathf.Cos((m_fAngled + DiffAngle) * Mathf.Deg2Rad);//计算y位置
+
+                transform.position = new Vector3(posX, posZ, 0) + AroundTarget.position;//更新位置   
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI/SplitBoss/SplitBossPartCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitBossPartCollision : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody;
+    private SplitBossPart m_part;
+    private bool m_bCollide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_part = GetComponent<SplitBossPart>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if( collision.gameObject.tag == "floor")
+        {
+            m_part.SetGravity();
+            m_bCollide = true;
+        }
+    }
+    public bool IsCollision()
+    {
+        return m_bCollide;
+    }
+}
