46169b87169c381ed6497b9ada0c1da988e6c717
kaleidosgu <gjcaoyi@163.com>
2019-10-23 20:41:33 +0800

    下坠飞行的敌人逻辑更改

--- Assets/Scripts/AI/Enemy_IronFoot.cs
+++ Assets/Scripts/AI/Enemy_IronFoot.cs
@@ -14,34 +14,80 @@ public class Enemy_IronFoot : Enemy_Base {
 	public float threshold=50f;
 	public bool isFalling=false;
 
+    public LayerMask FloorLayerMask;
+
+    public float RayCastDistance;
+    public float FallingSpeed;
+
+    private Collider2D m_collider2d;
+
+    private bool m_bFalling;
 	private void Awake() {
 		anim=GetComponent<Animator>();
 		rb = GetComponent<Rigidbody2D>();
-	}
-	void Start () {
-		
-	}
+
+    }
+    private bool _isHit()
+    {
+        bool bRes = false;
+        RaycastHit2D[] _arrayHit = Physics2D.RaycastAll(transform.position, -transform.up, RayCastDistance, FloorLayerMask, 0);
+        foreach (RaycastHit2D _hit in _arrayHit)
+        {
+            if (_hit.collider.gameObject != gameObject)
+            {
+                bRes = true;
+                break;
+            }
+        }
+        return bRes;
+    }
+	void Start ()
+    {
+        m_collider2d = GetComponent<Collider2D>();
+
+        m_bFalling = !_isHit();
+        _processFalling();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		if (rb.velocity.y<=-threshold)
-		{
-			Falling(true);
-		}else if(rb.velocity.y>=-5) Falling(false);
-	}
-	
-	void Falling(bool isFall){
-		if(isFall){
-			anim.CrossFade("Enemy_IronFoot",0.01f);
-			rb.gravityScale=0.1f;
-			rb.drag=0.2f;
-		}else
-		{
-			anim.CrossFade("Enemy_IronFoot_idle",0.01f);
-			rb.gravityScale=200;
-			rb.drag=0f;
-		}
-	}
+	void Update ()
+    {
+    }
+    private void FixedUpdate()
+    {
+        bool bNewFalling = !_isHit();
+        m_bFalling = bNewFalling;
+        _processFalling();
+    }
+    private void _processFalling()
+    {
+        if( m_bFalling == true )
+        {
+            anim.CrossFade("Enemy_IronFoot", 0.01f);
+            rb.gravityScale = 0;
+            rb.drag = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, -FallingSpeed);
+        }
+        else
+        {
+            anim.CrossFade("Enemy_IronFoot_idle", 0.01f);
+            rb.isKinematic = false;
+            rb.gravityScale = 200;
+            rb.drag = 0f;
+        }
+    }
+ //   void Falling(bool isFall){
+	//	if(isFall){
+	//		anim.CrossFade("Enemy_IronFoot",0.01f);
+	//		rb.gravityScale=0.1f;
+	//		rb.drag=0.2f;
+	//	}else
+	//	{
+	//		anim.CrossFade("Enemy_IronFoot_idle",0.01f);
+	//		rb.gravityScale=200;
+	//		rb.drag=0f;
+	//	}
+	//}
 
 
 
