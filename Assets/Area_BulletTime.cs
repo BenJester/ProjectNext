89b7bfe9129b42c1656ce803f62becbac2085e02
yzitao <yzitao@foxmail.com>
2019-10-23 12:00:07 +0800

    子弹空间

---
+++ Assets/Area_BulletTime.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area_BulletTime : MonoBehaviour
+{
+
+    private BulletTime bulletTime;
+    // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        bulletTime = GameObject.FindGameObjectWithTag("player").GetComponent<BulletTime>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            bulletTime.bulletTimeActive = true;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            bulletTime.bulletTimeActive = false;
+        }
+    }
+}
