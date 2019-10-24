0c0f1f1aa8c93d16e3ba0a63d723353548f57147
jeffyxie <jeffyxie@tencent.com>
2019-10-24 20:14:06 +0800

    no message

--- Assets/Scripts/Thing.cs
+++ Assets/Scripts/Thing.cs
@@ -38,6 +38,7 @@ public class Thing : MonoBehaviour {
     public float distanceToPlayer = Mathf.Infinity;
 
 	public virtual void Start () {
+        Debug.Log(gameObject.name+ transform.lossyScale);
 		player = GameObject.FindWithTag ("player");
 		playerControl = player.GetComponent<PlayerControl1> ();
 		originalScale = transform.localScale;
