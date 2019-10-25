a44e5a586509573d964f4e373b5ecbcb41078e02
kaleidosgu <gjcaoyi@163.com>
2019-10-25 14:52:57 +0800

    触碰怪物毁灭，扣除怪物血以及召唤出生成物的组件添加

---
+++ Assets/Scripts/common/SpawnObjAfterDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjAfterDestroy : MonoBehaviour
+{
+    public List<GameObject> LstSpawnObj;
+    // Start is called before the first frame update
+    public void SpawnObject()
+    {
+        foreach(GameObject objIns in LstSpawnObj)
+        {
+            Instantiate(objIns, transform.position, Quaternion.identity);
+        }
+    }
+}
