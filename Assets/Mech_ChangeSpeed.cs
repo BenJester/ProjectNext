793fe0f083605cdc4324aa4c3137cc6e7a22460d
yzitao <yzitao@foxmail.com>
2019-10-25 11:15:57 +0800

    加入弹簧的动画

--- Assets/Mech_ChangeSpeed.cs
+++ Assets/Mech_ChangeSpeed.cs
@@ -7,20 +7,28 @@ public class Mech_ChangeSpeed : MonoBehaviour
     [Header("Dir为零时原路反弹，否则按Dir方向弹出")]
     public Vector2 dir;
     public float speedFactor;
+    public Animator anim;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D col)
     {
+        
+
         if (col.tag == "bullet")
             return;
         Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
         float speed = rb.velocity.magnitude;
+
+        
+
         if (dir != Vector2.zero)
         {
             rb.velocity = dir * speed*speedFactor;
+            anim.CrossFade("Mech_Tanhuang", 0.01f);
         }
         else if(speedFactor!=0)
         {
             rb.velocity = -rb.velocity * speedFactor;
+            anim.CrossFade("Mech_Tanhuang", 0.01f);
         }
 
         if (col.gameObject.CompareTag("player"))
@@ -34,6 +42,8 @@ public class Mech_ChangeSpeed : MonoBehaviour
             {
                 Debug.Assert(false, string.Format("Player has not playerstatemanager component"));
             }
+
+            anim.CrossFade("Mech_Tanhuang", 0.01f);
         }
     }
 }
