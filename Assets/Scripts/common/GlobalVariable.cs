7eeaceabe31d9d18b0e00e260c84e69f7fc89530
kaleidosgu <gjcaoyi@163.com>
2019-10-28 10:53:55 +0800

    全局变量添加

---
+++ Assets/Scripts/common/GlobalVariable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariable 
+{
+    public static UIPlayerController s_UIPlayerCtrl;
+
+    public static UIPlayerController GetUIPlayerCtrl()
+    {
+        if( s_UIPlayerCtrl == null )
+        {
+            s_UIPlayerCtrl = GameObject.FindObjectOfType<UIPlayerController>();
+        }
+        return s_UIPlayerCtrl;
+    }
+}
