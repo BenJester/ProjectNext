c2f4a43b2acb521c85a79612b05c73b2d18b3e54
yzitao <yzitao@foxmail.com>
2019-11-04 16:33:44 +0800

    加入两个新的area prefab 1. 一个无限冲刺，2. 一个加速器

---
+++ Assets/Area_Acceleration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area_Acceleration : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float velocityMultiple=1.05f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            collision.GetComponent<Rigidbody2D>().velocity *= velocityMultiple;
+        }
+
+
+    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "player")
+    //    {
+    //        collision.GetComponent<Rigidbody2D>().velocity *= velocityMultiple;
+    //    }
+        
+
+    //}
+}
