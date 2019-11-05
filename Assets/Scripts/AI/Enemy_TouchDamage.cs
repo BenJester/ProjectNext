365549ff305d878afc05241e7984405cad41303d
kaleidosgu <gjcaoyi@163.com>
2019-11-05 19:40:55 +0800

    跟踪boss添加

---
+++ Assets/Scripts/AI/Enemy_TouchDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_TouchDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if( collision.CompareTag(GlobalTagDefine.TagName_player) == true )
+        {
+            PlayerControl1 _ctrl = collision.GetComponent<PlayerControl1>();
+            if( _ctrl != null )
+            {
+                _ctrl.Die();
+                StartCoroutine(_ctrl.DelayRestart());
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/AI/TracingBoss.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracingBoss : MonoBehaviour
+{
+    public float DistanceDashPlayer;
+    public float DistanceDelta;
+    private MovingObjectByWayPoint m_movingObj;
+    private bool m_bDash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_movingObj = GetComponent<MovingObjectByWayPoint>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(m_bDash == false)
+        {
+            float fDistancePlayer = Vector2.Distance(transform.position, GlobalVariable.GetPlayer().transform.position);
+            if (fDistancePlayer <= DistanceDashPlayer)
+            {
+                m_bDash = true;
+                m_movingObj.enabled = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, GlobalVariable.GetPlayer().transform.position, DistanceDelta * Time.fixedDeltaTime);
+        }
+    }
+}
