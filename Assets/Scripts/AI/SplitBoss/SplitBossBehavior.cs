917cb8ce3c74f86c800fc4cb32db7d863226cae1
kaleidosgu <gjcaoyi@163.com>
2019-11-07 10:13:35 +0800

    把collider的位移从动画里面更改出来

--- Assets/Scripts/AI/SplitBoss/SplitBossBehavior.cs
+++ Assets/Scripts/AI/SplitBoss/SplitBossBehavior.cs
@@ -15,9 +15,10 @@ public class SplitBossBehavior : MonoBehaviour
     private SplitBossJump m_jump;
     private SplitBossComponent m_bossCom;
     private SplitBossShoot m_shootBoss;
+    private EnemySkillShoot m_shootSkill;
     private bool m_bJumping;
     private float m_fCurrentReadyShoot;
-    private bool m_bShoot;
+    public bool m_bShoot;
 
     public bool testCallBack;
     // Start is called before the first frame update
@@ -26,61 +27,68 @@ public class SplitBossBehavior : MonoBehaviour
         m_jump = GetComponent<SplitBossJump>();
         m_bossCom = GetComponent<SplitBossComponent>();
         m_shootBoss = GetComponent<SplitBossShoot>();
-        m_jump.JumpBoss();
-        m_bJumping = true;
+        m_shootSkill = GetComponent<EnemySkillShoot>();
+        //m_jump.JumpBoss();
+        //m_bJumping = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(m_bJumppingRight == true)
+        if(m_bShoot == true)
         {
-
+            m_bShoot = false;
+            m_shootSkill.CastSkill();
+            //Debug.Assert(false);
         }
     }
 
     private void FixedUpdate()
     {
-        if(m_bJumping == true)
-        {
-            if (m_nCurJumpingCounts >= JumpingCounts)
-            {
-                //m_nCurJumpingCounts = 0;
-                m_bossCom.FlipBoss(!m_bossCom.BossRight());
-                m_jump.StopJump();
-                m_bJumping = false;
-                m_bShoot = true;
-            }
-            else
-            {
-                m_fCurJumpingTime += Time.fixedDeltaTime;
-                if (m_fCurJumpingTime >= JumpingTime)
-                {
-                    m_fCurJumpingTime -= JumpingTime;
-                    m_nCurJumpingCounts++;
-                    m_jump.JumpBoss();
-                }
-            }
-        }
         if(m_bShoot == true)
         {
-            m_fCurrentReadyShoot += Time.fixedDeltaTime;
-            if( m_fCurrentReadyShoot >= TimeToShootAfterJump )
-            {
-                m_shootBoss.BossShoot();
-                m_bShoot = false;
-            }
-        }
 
-        if(testCallBack == true)
-        {
-            testCallBack = false;
-            foreach (SplitBossPart _part in LstParts)
-            {
-                _part.ThingEnable(false);
-                _part.GetComponent<RotateByTarget>().StopRotate();
-                _part.GetComponent<SplitCallBackParts>().StartCallBack(_part.TransOriginalSplit.transform, 1);
-            }
         }
+        //if(m_bJumping == true)
+        //{
+        //    if (m_nCurJumpingCounts >= JumpingCounts)
+        //    {
+        //        //m_nCurJumpingCounts = 0;
+        //        m_bossCom.FlipBoss(!m_bossCom.BossRight());
+        //        m_jump.StopJump();
+        //        m_bJumping = false;
+        //        m_bShoot = true;
+        //    }
+        //    else
+        //    {
+        //        m_fCurJumpingTime += Time.fixedDeltaTime;
+        //        if (m_fCurJumpingTime >= JumpingTime)
+        //        {
+        //            m_fCurJumpingTime -= JumpingTime;
+        //            m_nCurJumpingCounts++;
+        //            m_jump.JumpBoss();
+        //        }
+        //    }
+        //}
+        //if(m_bShoot == true)
+        //{
+        //    m_fCurrentReadyShoot += Time.fixedDeltaTime;
+        //    if( m_fCurrentReadyShoot >= TimeToShootAfterJump )
+        //    {
+        //        m_shootBoss.BossShoot();
+        //        m_bShoot = false;
+        //    }
+        //}
+
+        //if(testCallBack == true)
+        //{
+        //    testCallBack = false;
+        //    foreach (SplitBossPart _part in LstParts)
+        //    {
+        //        _part.ThingEnable(false);
+        //        _part.GetComponent<RotateByTarget>().StopRotate();
+        //        _part.GetComponent<SplitCallBackParts>().StartCallBack(_part.TransOriginalSplit.transform, 1);
+        //    }
+        //}
     }
 }
---
+++ Assets/Scripts/Collider/ColliderOffsetComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class ColliderOffsetComponent : MonoBehaviour
+{
+    public Vector2 DestOfMovement;
+    public float SpeedOfOffset;
+
+    private Collider2D m_collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_collider.offset = Vector2.MoveTowards(m_collider.offset, DestOfMovement, SpeedOfOffset);
+    }
+}
