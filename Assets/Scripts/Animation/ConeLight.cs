ca16d143fedf7361d51880313c4518bda5bf23b8
BenJester <jichu.yang1998@gmail.com>
2019-11-10 00:21:24 -0500

    增加手电筒光效

---
+++ Assets/Scripts/Animation/ConeLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float angleOffset = 0f;
+    Transform player;
+    void Start()
+    {
+        player = transform.parent;
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position;
+        Vector3 vecMouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(vecMouseWorldPos);
+        float angleToCursor = Mathf.Rad2Deg * PlayerControl1.AngleBetween(player.position, vecMouseWorldPos);
+        transform.eulerAngles = new Vector3(0f, 0f, angleToCursor + angleOffset);
+    }
+}
