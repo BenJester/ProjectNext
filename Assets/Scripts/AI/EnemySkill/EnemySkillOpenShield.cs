da0318f862dc13dad2608f222b6e5a3a2a2f1386
kaleidosgu <gjcaoyi@163.com>
2019-11-18 11:48:58 +0800

    敵人打開打開盾的技能以及行爲樹的更改

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillOpenShield.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkillOpenShield : EnemySkillBase
+{
+    private Thing m_thing;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+        m_thing = GetComponent<Thing>();
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+        m_thing.hasShield = true;
+    } 
+}
