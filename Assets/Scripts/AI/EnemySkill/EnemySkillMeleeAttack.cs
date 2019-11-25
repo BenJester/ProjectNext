0ea43ac1a65f0d8118af57333593943f65acb582
kaleidosgu <gjcaoyi@163.com>
2019-11-25 16:53:26 +0800

    怪物的近战相关添加

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillMeleeAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class MyThingUnityEvent : UnityEvent<Thing>
+{
+}
+[System.Serializable]
+public class MyPlayerUnityEvent : UnityEvent<PlayerControl1>
+{
+}
+public class EnemySkillMeleeAttack : EnemySkillBase
+{
+    public GameObject MeleeGameObject;
+    public Vector3 OffsetWithParent;
+
+    public UnityEvent<Thing> ua;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+
+        GameObject _insMelee = Instantiate(MeleeGameObject, Vector3.zero, Quaternion.identity);
+        _insMelee.transform.SetParent(transform);
+        _insMelee.transform.localPosition = OffsetWithParent;
+    } 
+}
