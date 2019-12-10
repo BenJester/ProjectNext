b9d6efd0b01d8c60b616cde54cdb9c225678edbe
yzitao <yzitao@foxmail.com>
2019-12-10 20:18:26 +0800

    修复door的lr显示

---
+++ Assets/ShowLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public LineRenderer lr;
+    void Start()
+    {
+        lr.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            lr.enabled = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "player")
+        {
+            lr.enabled = false;
+        }
+    }
+}
