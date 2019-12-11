99b36d86e66774e35877f7d126e9d0fd9de145c8
kaleidos <gjcaoyi@163.com>
2019-12-11 14:10:40 +0800

    漏传第二次交换的代码

---
+++ Assets/Scripts/PlayerScript/PlayerDoubleSwap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDoubleSwap : MonoBehaviour
+{
+    public bool DoubleSwap;
+    public GameObject DoubleSwapMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DoubleSwap == false)
+        {
+            DoubleSwapMarker.gameObject.SetActive(false);
+        }
+    }
+    public void ProcessDoubleSwap(Swap _swap)
+    {
+        if (DoubleSwap == true)
+        {
+            if (_swap.CanDoubleSwap())
+            {
+                if (_swap.col != null && !_swap.col.GetComponent<Thing>().dead)
+                {
+                    GameObjectUtil.SafeSetActive(true, DoubleSwapMarker);
+                    DoubleSwapMarker.transform.position = new Vector3(_swap.col.transform.position.x, _swap.col.gameObject.transform.position.y, -1f);
+                }
+                else
+                {
+                    GameObjectUtil.SafeSetActive(false, DoubleSwapMarker);
+                }
+            }
+            else
+            {
+                GameObjectUtil.SafeSetActive(false, DoubleSwapMarker);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Util/GameObjectUtil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectUtil 
+{
+    public static void SafeSetActive(bool bActive,GameObject obj)
+    {
+        if(bActive == true)
+        {
+            if(obj.activeSelf == false)
+            {
+                obj.SetActive(true);
+            }
+        }
+        else
+        {
+            if (obj.activeSelf == true)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+}
