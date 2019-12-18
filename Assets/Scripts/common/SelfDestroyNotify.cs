44b95872a190f6a872beccc9c4ff85ef19534fdb
kaleidos <gjcaoyi@163.com>
2019-12-18 15:15:25 +0800

    标记毁灭后需要告知二次交换

---
+++ Assets/Scripts/common/SelfDestroyNotify.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SelfDestroyNotify : MonoBehaviour
+{
+    public UnityEvent DestroyDedicator;
+    // Start is called before the first frame update
+    private void OnDestroy()
+    {
+        if(DestroyDedicator != null)
+        {
+            DestroyDedicator.Invoke();
+        }
+    }
+}
