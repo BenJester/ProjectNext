8822fb8072106996a884392717dd4062a68654de
yzitao <yzitao@foxmail.com>
2019-12-18 11:48:42 +0800

    两关二次交换关卡+一个多Mech按钮功能组件制作

---
+++ Assets/Mech_MultiMechs_Button.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mech_MultiMechs_Button : MonoBehaviour
+{
+    public bool onlyOnce;
+    public bool isPressed = false;
+    public Mech_base[] mechs;
+    //public LineRenderer lr;
+    private Vector3 oriPos;
+    private float cd;
+
+    void Start()
+    {
+        //lr.SetPosition(0, transform.position);
+        //lr.SetPosition(1, mech.transform.position);
+        oriPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            foreach (var mech in mechs)
+            {
+                if (mech.type == Mech_base.MechType.Doing)
+                {
+                    mech.Doing();
+                }
+            }
+           
+        }
+
+        if (cd > 0)
+        {
+            cd -= Time.deltaTime;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((!isPressed && collision.tag == "thing") || (!isPressed && collision.tag == "player"))
+        {
+            isPressed = true;
+            transform.position = oriPos - new Vector3(0, 10, 0);
+
+            if (cd <= 0)
+            {
+                foreach (var mech in mechs)
+                {
+                    mech.DoOnce();
+                }
+                
+                if (onlyOnce)
+                    Destroy(gameObject);
+                cd = 1;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((isPressed && collision.tag == "thing") || (isPressed && collision.tag == "player"))
+        {
+            isPressed = false;
+            transform.position = oriPos;
+        }
+    }
+}
