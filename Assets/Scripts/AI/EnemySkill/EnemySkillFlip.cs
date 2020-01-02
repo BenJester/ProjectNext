e36381427d7e305159bee2c3a6f21f09ba0391dc
kaleidos <gjcaoyi@163.com>
2020-01-02 11:05:32 +0800

    添加翻转角色的技能

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillFlip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkillFlip : EnemySkillBase
+{
+    public bool RightDir;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+        if(RightDir == true)
+        {
+            transform.localRotation = Quaternion.AngleAxis(0, Vector2.up);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.AngleAxis(180, Vector2.up);
+        }
+    } 
+}
