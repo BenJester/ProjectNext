0dd5d9f19fd915746669b808fc67e80734b2054a
kaleidos <gjcaoyi@163.com>
2020-01-02 10:46:01 +0800

    添加怪物模板

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkillMove : EnemySkillBase
+{
+    public float MoveForwardForce;
+    public float MoveTime;
+    private bool m_bMoving;
+    private float m_fMovingTime;
+    private Rigidbody2D m_rigid;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+        m_rigid = GetComponent<Rigidbody2D>();
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+        m_bMoving = true;
+        m_fMovingTime = 0.0f;
+    }
+    private void FixedUpdate()
+    {
+        if (m_bMoving == true)
+        {
+            m_fMovingTime += Time.fixedDeltaTime;
+            m_rigid.AddForce(transform.right * MoveForwardForce);
+            if (m_fMovingTime >= MoveTime)
+            {
+                m_bMoving = false;
+            }
+        }
+    }
+}
