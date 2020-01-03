d9d39c5f0bea4dd804032ab51f21b206acee2d83
kaleidos <gjcaoyi@163.com>
2020-01-03 10:07:23 +0800

    自我翻转功能添加

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillFlipSelf.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkillFlipSelf : EnemySkillBase
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+        if( transform.eulerAngles.y == 0)
+        {
+            transform.localRotation = Quaternion.AngleAxis(180, Vector2.up);
+        }
+        else if (transform.eulerAngles.y == 180)
+        {
+            transform.localRotation = Quaternion.AngleAxis(0, Vector2.up);
+        }
+    }
+}
