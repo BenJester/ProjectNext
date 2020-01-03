fe95cd2734043608fbb1f4328563a8f17efe52d3
kaleidos <gjcaoyi@163.com>
2020-01-03 11:54:17 +0800

    碰触消失

---
+++ Assets/Scripts/common/SkillAction/CollisionDisappear.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDisappear : MonoBehaviour
+{
+    public bool CollisionPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(CollisionPlayer == true)
+        {
+            if (collision.gameObject.CompareTag(GlobalTagDefine.TagName_player))
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+}
