f74f97488807096a50f21b38233ae01b0b71d827
kaleidos <gjcaoyi@163.com>
2020-01-03 16:34:19 +0800

    添加删除自身和检测thing

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillDestroySelf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkillDestroySelf : EnemySkillBase
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+        Destroy(gameObject);
+    } 
+}
