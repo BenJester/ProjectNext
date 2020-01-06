b6f29e6882001e04e93f5bed9184ded9ffb0892a
kaleidos <gjcaoyi@163.com>
2020-01-06 14:36:19 +0800

    更改rigidbody的功能添加

---
+++ Assets/Scripts/AI/EnemySkill/EnemySkillChangeRigidbody.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkillChangeRigidbody : EnemySkillBase
+{
+    public RigidbodyType2D bodyTyp;
+
+    public float DynamicMass;
+    public float DynamicGravity;
+
+    private Rigidbody2D m_rigidbody;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Registe(this);
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        if(m_rigidbody == null)
+        {
+            Debug.Assert(false, "EnemySkillChangeRigidbody rigidbody is null");
+        }
+    }
+    public override void CastSkill()
+    {
+        base.CastSkill();
+        m_rigidbody.bodyType = bodyTyp;
+        if (bodyTyp == RigidbodyType2D.Dynamic)
+        {
+            m_rigidbody.mass = DynamicMass;
+            m_rigidbody.gravityScale = DynamicGravity;
+        }
+    } 
+}
