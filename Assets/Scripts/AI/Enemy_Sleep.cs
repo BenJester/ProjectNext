79a2bde251ed85525d193e597229541e1af11abe
BenJester <jichu.yang1998@gmail.com>
2020-02-12 20:33:41 +0800

    sleep enemy logic

---
+++ Assets/Scripts/AI/Enemy_Sleep.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Sleep : MonoBehaviour
+{
+    public float explodeDelay;
+    public float sleepDelay;
+    public float explodeRadius;
+    Vector3 prevPos;
+    public float wakeUpThreshold;
+    bool wokeUp;
+
+    void Start()
+    {
+        prevPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (prevPos != transform.position && (prevPos - transform.position).magnitude < wakeUpThreshold && !wokeUp)
+        {
+            wokeUp = true;
+            StartCoroutine(Explode());
+        }
+        else if (wokeUp && prevPos == transform.position)
+        {
+            StartCoroutine(Sleep());
+        }
+        prevPos = transform.position;
+    }
+
+    IEnumerator Sleep()
+    {
+        yield return new WaitForSeconds(sleepDelay);
+        wokeUp = false;
+    }
+
+    IEnumerator Explode()
+    {
+        
+        yield return new WaitForSeconds(explodeDelay);
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, explodeRadius);
+        foreach (var col in cols)
+        {
+            if (col.CompareTag("thing") && col.gameObject != gameObject)
+            {
+                if (col.GetComponent<Thing>().type != Ben.Type.box)
+                    col.GetComponent<Thing>().Die();
+            }
+            else if (col.CompareTag("player"))
+            {
+                PlayerControl1.Instance.Die();
+            }
+        }
+    }
+}
