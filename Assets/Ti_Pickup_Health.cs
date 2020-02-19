74972ca6c9a184166804ae889b657304419e0b1b
zitaoye <464186281@qq.com>
2020-02-19 11:57:09 +0800

    pickup

---
+++ Assets/Ti_Pickup_Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ti_Pickup_Health : TriggerItem_Base
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void HandleKickTrigger(){
+        for (int i = 0; i < count; i++)
+        {
+            GameObject Object= Instantiate(instance,transform.position,Quaternion.identity);
+            Object.transform.parent=null;
+        }
+        Destroy(gameObject);
+
+    }
+
+    public override void HandleSwapTrigger(){
+        for (int i = 0; i < count; i++)
+        {
+            GameObject Object= Instantiate(instance,transform.position,Quaternion.identity);
+            Object.transform.parent=null;
+        }
+        Destroy(gameObject);
+    }
+
+}
