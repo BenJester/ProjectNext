09a1d9bd278e5c915cd244917599c5ff83121f88
zitaoye <464186281@qq.com>
2020-02-21 17:11:22 +0800

    BAGsYSTEM PRESS F

---
+++ Assets/BagSystem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isCollect = false;
+    public float collectRadius;
+    public GameObject bagFirstThing;
+    public GameObject indicator;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!isCollect)
+            {
+                StartCoroutine(StartCollection());
+            }
+            else
+            {
+                Set();
+            }
+        }
+    }
+
+
+
+    IEnumerator StartCollection(){
+        GameObject area = Instantiate(indicator, transform.position, Quaternion.identity);
+        area.transform.parent = null;
+        area.GetComponent<SpriteRenderer>().size = new Vector2(collectRadius * 2, collectRadius * 2);
+        area.transform.parent = transform;
+        yield return new WaitForSeconds(0.5f);
+        area.GetComponent<SpriteRenderer>().color=Color.green;
+        Destroy(area,0.1f);
+        Collect();
+    }
+        
+
+    
+    public void Collect()
+    {
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, collectRadius, 1<<12);
+       
+
+        foreach (var item in cols)
+        {
+            if (item.tag == "thing")
+            {
+                bagFirstThing = item.gameObject;
+                bagFirstThing.transform.position = (Vector2)transform.position + Vector2.up * 100000;
+                isCollect = true;
+                return;
+            }
+        }
+    }
+
+    public void Set()
+    {
+        if (bagFirstThing != null && isCollect)
+        {
+            bagFirstThing.transform.position = (Vector2)transform.position + Vector2.up * 50;
+            isCollect = false;
+        }
+    }
+
+}
--- Assets/Ti_Gun.cs
+++ Assets/Ti_Gun.cs
@@ -2,47 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ti_Gun : TriggerItem_Base
+public class Ti_Gun : Ti_GunType
 {
     // Start is called before the first frame update
-    
 
-    public bool isRight=true;
-     Vector2 direction;
+
+    public bool isRight = true;
+    Vector2 direction;
     public GameObject bullet;
     public float bulletSpeed;
     public float bulletInstanceDistance = 50f;
 
     void Start()
     {
-      if(!isRight){
-        GetComponent<SpriteRenderer>().flipX=true;
-      }
+        base.Start();
+        if (!isRight)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public override void HandleKickTrigger(){
+    public override void HandleKickTrigger()
+    {
         StartCoroutine(Shoot());
     }
 
-     public override void HandleSwapTrigger(){
+    public override void HandleSwapTrigger()
+    {
         StartCoroutine(Shoot());
     }
 
-    IEnumerator Shoot(){
-        yield return new WaitForSeconds(0.2f);
-        //Dir
-        if(isRight) direction = transform.right;
-        else direction = -transform.right;
-        //  
+    IEnumerator Shoot()
+    {
 
-        GameObject newBullet = Instantiate (bullet, transform.position + bulletInstanceDistance * (Vector3) direction, Quaternion.identity);
-		Rigidbody2D bulletBody = newBullet.GetComponent<Rigidbody2D> ();
-		bulletBody.velocity = direction * bulletSpeed;
+        if (ammoNow > 0)
+        {
+            yield return new WaitForSeconds(0.2f);
+            //Dir
+            if (isRight) direction = transform.right;
+            else direction = -transform.right;
+            //  
+            GameObject newBullet = Instantiate(bullet, transform.position + bulletInstanceDistance * (Vector3)direction, Quaternion.identity);
+            Rigidbody2D bulletBody = newBullet.GetComponent<Rigidbody2D>();
+            bulletBody.velocity = direction * bulletSpeed;
+
+            ammoNow -= 1;
+            SetAmmo();
+
+        }
+
+
+
     }
 }
