26b0c041e8517f88a1bb3766e7d504f542ce6afb
BenJester <jichu.yang1998@gmail.com>
2020-02-22 22:34:00 +0800

    bag system

--- Assets/BagSystem.cs
+++ Assets/BagSystem.cs
@@ -7,29 +7,59 @@ public class BagSystem : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool isCollect = false;
-    public Image itemImage;
+    public List<Image> itemImages;
     public float collectRadius;
-    public GameObject bagFirstThing;
+    public List<GameObject> items;
+    public int index;
     public GameObject indicator;
+    PlayerControl1 playerControl;
+    public float throwSpeed;
+    public float throwOffset;
+    public LayerMask colletLayer;
+    public Image border;
+    public Sprite empty;
     void Start()
     {
-        itemImage.sprite=null;
+        playerControl = PlayerControl1.Instance;
+        for (int i = 0; i < itemImages.Count; i ++)
+        {
+            if (items[i] != null)
+                itemImages[i].sprite = items[i].GetComponent<SpriteRenderer>().sprite;
+            else
+                itemImages[i].sprite = empty;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetMouseButtonUp(1))
         {
-            if (!isCollect)
+            if (items[index] == null)
             {
                 StartCoroutine(StartCollection());
             }
             else
             {
-                Set();
+                Throw();
             }
         }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            index = 0;
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            index = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            index = 2;
+        }
+        border.rectTransform.position = itemImages[index].rectTransform.position;
     }
 
 
@@ -39,7 +69,7 @@ public class BagSystem : MonoBehaviour
         area.transform.parent = null;
         area.GetComponent<SpriteRenderer>().size = new Vector2(collectRadius * 2, collectRadius * 2);
         area.transform.parent = transform;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.1f);
         area.GetComponent<SpriteRenderer>().color=Color.green;
         Destroy(area,0.1f);
         Collect();
@@ -49,30 +79,54 @@ public class BagSystem : MonoBehaviour
     
     public void Collect()
     {
-        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, collectRadius, 1<<12);
+        if (items[index] != null) return;
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, collectRadius, colletLayer);
        
 
         foreach (var item in cols)
         {
-            if (item.tag == "thing")
-            {
-                bagFirstThing = item.gameObject;
-                bagFirstThing.transform.position = (Vector2)transform.position + Vector2.up * 100000;
-                itemImage.sprite = item.GetComponent<SpriteRenderer>().sprite;
-                isCollect = true;
-                return;
-            }
+            items[index] = item.gameObject;
+            items[index].transform.position = (Vector2)transform.position + Vector2.up * 100000;
+            itemImages[index].sprite = item.GetComponent<SpriteRenderer>().sprite;
+            isCollect = true;
+            return;
         }
     }
 
-    public void Set()
+    public void Throw()
     {
-        if (bagFirstThing != null && isCollect)
-        {
-            bagFirstThing.transform.position = (Vector2)transform.position + Vector2.up * 50;
-            itemImage.sprite=null;
-            isCollect = false;
-        }
+        if (items[index] == null) return;
+        StartCoroutine(DoThrow(items[index].GetComponent<Rigidbody2D>()));
     }
+
+    IEnumerator DoThrow(Rigidbody2D rb)
+    {
+        if (rb.GetComponent<Thing>().hasShield) yield break;
+
+        int layer = rb.gameObject.layer;
+        rb.gameObject.layer = 19;
+
+        items[index] = null;
+        itemImages[index].sprite = empty;
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = (mouseWorldPos - (Vector2)playerControl.transform.position).normalized;
+        rb.transform.position = playerControl.transform.position + (Vector3) dir * throwOffset;
+
+        rb.gameObject.GetComponent<Rigidbody2D>().velocity = dir * throwSpeed;
+        
+
+        yield return new WaitForSeconds(0.15f);
+        rb.gameObject.layer = layer;
+
+    }
+    //public void Set()
+    //{
+    //    if (bagFirstThing != null && isCollect)
+    //    {
+    //        bagFirstThing.transform.position = (Vector2)transform.position + Vector2.up * 50;
+    //        itemImage.sprite=null;
+    //        isCollect = false;
+    //    }
+    //}
 
 }
