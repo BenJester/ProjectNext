ef1f379cf37ff1f41e1d2815ac5d66b9b9dfeee5
BenJester <jichu.yang1998@gmail.com>
2020-02-25 18:03:43 +0800

    swap room teleport

---
+++ Assets/Scripts/SwapPuzzle/SwapPuzzleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapPuzzleManager : MonoBehaviour
+{
+    public static SwapPuzzleManager Instance { get; private set; }
+    public int currIndex;
+    public SwapPuzzleRoom[] roomList;
+    public float delay;
+
+    void Start()
+    {
+        transform.parent = null;
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+        roomList = GetComponentsInChildren<SwapPuzzleRoom>();
+        
+    }
+
+    
+    public IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(delay);
+        if (currIndex + 1 >= roomList.Length) yield break;
+        currIndex += 1;
+        PlayerControl1.Instance.transform.position = roomList[currIndex].transform.position;
+    }
+}
