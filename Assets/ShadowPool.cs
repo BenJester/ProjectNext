f12a28ea7c21fdb326fe739c531e6212154283cf
zitaoye <464186281@qq.com>
2020-03-02 17:28:23 +0800

    shadow

---
+++ Assets/ShadowPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowPool : MonoBehaviour
+{
+    public static ShadowPool instance;
+    public GameObject shadowPrefab;
+    public int shadowCount;
+
+    Queue<GameObject> availableObject = new Queue<GameObject>();
+
+    void Awake()
+    {
+        if(instance==null) instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)){
+            GetFromPool();
+        }
+    }
+
+    public void FillPool(){
+
+        for (int i = 0;i < shadowCount; i++) {
+            var newShadow = Instantiate(shadowPrefab);
+            newShadow.transform.SetParent(transform);
+
+            //取消启用，返回对象池
+            ReturnPool(newShadow);
+        }
+    }
+
+    public void ReturnPool(GameObject go){
+        go.SetActive(false);
+        availableObject.Enqueue(go);
+    }
+
+    public GameObject GetFromPool(){
+        if(availableObject.Count == 0){
+            FillPool();
+        }
+        var outShadow = availableObject.Dequeue();
+        outShadow.SetActive(true);
+        return outShadow;
+
+    }
+}
--- Assets/ShadowSprite.cs
+++ Assets/ShadowSprite.cs
@@ -27,18 +27,29 @@ public class ShadowSprite : MonoBehaviour
 
     void OnEnable()
     {
-        player = PlayerControl1.Instance.GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
         thisSprite = GetComponent<SpriteRenderer>();
-        playerSprite = player.GetComponent<SpriteRenderer>();
+        playerSprite = player.GetChild(0).GetComponent<SpriteRenderer>();
 
         alpha = alphaSet;
         thisSprite.sprite = playerSprite.sprite;
         transform.position = player.position;
+        transform.localScale = player.localScale;
+        transform.rotation = player.rotation;
+
+        activeStart = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        alpha *= alphaMultiplier;
+        color = new Color(.5f,.5f,1,alpha);
+        thisSprite.color = color;
+        if(Time.time >= activeStart + activeTime){
+            //return to object pool
+            ShadowPool.instance.ReturnPool(this.gameObject);
+        }
     }
+
 }
