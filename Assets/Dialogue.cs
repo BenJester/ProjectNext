c2e248b82f06fac68dd265eb2c652d6c2859eb12
zitaoye <464186281@qq.com>
2020-03-06 12:38:55 +0800

    dialogue

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue{
+
+
+    public Sprite photo;
+    public string name;
+
+    [TextArea(3,10)]
+    public string[] sentences;
+}
---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    public Text dialogueText;
+    public Image photoImage;
+    public Animator animator;
+
+    //FIFO 
+    Queue<string> sentences;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+
+
+        animator.SetBool("isOpen", true);
+        Debug.Log("starting conversation with" + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        photoImage.sprite = dialogue.photo;
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("End the Conversation");
+    }
+
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence)
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+
+}
