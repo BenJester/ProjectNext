5cc9feeafc72608f6e33de99f9f85b545810427c
BenJester <jichu.yang1998@gmail.com>
2020-03-09 16:02:23 +0800

    移动人质

--- Assets/Scripts/Hostage.cs
+++ Assets/Scripts/Hostage.cs
@@ -7,11 +7,14 @@ public class Hostage : MonoBehaviour {
 	Thing thing;
 	public float dropKillSpeed;
 	Goal goal;
+    public float speed;
+    Rigidbody2D rb;
 
 	void Start () {
 		thing = GetComponent<Thing> ();
 		goal = GameObject.FindGameObjectWithTag ("goal").GetComponent<Goal>();
 		goal.hostageList.Add (GetComponent<Thing> ());
+        rb = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
@@ -21,8 +24,14 @@ public class Hostage : MonoBehaviour {
 			thing.Die();
 		}
 	}
-
-	void OnCollisionEnter2D (Collision2D col) {
+    private void FixedUpdate()
+    {
+        if (speed != 0f)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+    }
+    void OnCollisionEnter2D (Collision2D col) {
 
 		if (thing.prevVelocity.y < -dropKillSpeed && col.transform.position.y < transform.position.y) {
 			thing.collider.enabled = false;
