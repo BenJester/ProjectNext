82c526496a43658a9c960ebe768ab5b1579c7bdd
BenJester <jichu.yang1998@gmail.com>
2020-03-11 16:53:08 +0800

    swap trigger event

---
+++ Assets/Scripts/LevelObjects/SwapTriggerEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwapTriggerEvent : MonoBehaviour
+{
+    public UnityEvent TriggerEvent;
+
+    void Start()
+    {
+        PlayerControl1.Instance.swap.OnSwap += HandleSwap;
+    }
+    void HandleSwap()
+    {
+        TriggerEvent?.Invoke();
+    }
+
+    void Update()
+    {
+        
+    }
+}
