c64e0c509a537b6b2594a0112ed3c39ed583346b
yzitao <yzitao@foxmail.com>
2020-03-16 14:34:29 +0800

    jumping box

---
+++ Assets/Ti_JumpingBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ti_JumpingBox : MonoBehaviour
+{
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddForceToPlayer() {
+        PlayerControl1.Instance.GetComponent<Rigidbody2D>().AddForce(Vector2.up*force);
+    }
+}
