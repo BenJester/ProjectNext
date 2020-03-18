2bdde253f02bb8fd9fdc6fe31bca349549f29b31
BenJester <jichu.yang1998@gmail.com>
2020-03-18 11:59:45 +0800

    踩敌人头上跳

---
+++ Assets/Scripts/Skills/JumpOnEnemies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOnEnemies : Skill
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Enemy>() != null & collision.collider.transform.position.y < transform.position.y)
+            StartCoroutine(LateJump());
+    }
+
+    IEnumerator LateJump()
+    {
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        playerBody.velocity = new Vector2(playerBody.velocity.x, playerControl.jumpSpeed);
+    }
+}
