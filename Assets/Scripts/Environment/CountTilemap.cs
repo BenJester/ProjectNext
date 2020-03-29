127687df89d7ca90bdb0e120fd3ee62a12eaeade
kaleidos <gjcaoyi@163.com>
2020-03-29 10:22:06 +0800

    更改starter场景的地图tile，更改tile的xml文件读取

--- Assets/Scripts/Environment/CountTilemap.cs
+++ Assets/Scripts/Environment/CountTilemap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Text;
+using System.Xml;
 [System.Serializable]
 public class ReplaceString
 {
@@ -11,16 +12,23 @@ public class ReplaceString
 }
 public class CountTilemap : MonoBehaviour
 {
-    public Tilemap AssignedTitleMap;
+    public List<Tilemap> LstAssignedTitleMap;
     public List<ReplaceString> LstReplaceString;
     public bool NeedReplace;
+    public string GenerateFileName;
+
     private Dictionary<string, string> m_dicReplace;
     private int m_nMaxWidth;
     private int m_nMaxHeight;
     // Start is called before the first frame update
     void Start()
     {
-        if(NeedReplace == true)
+        _replaceInit();
+        int a = 0;
+    }
+    private void _replaceInit()
+    {
+        if (NeedReplace == true)
         {
             m_dicReplace = new Dictionary<string, string>();
             foreach (ReplaceString str in LstReplaceString)
@@ -42,9 +50,135 @@ public class CountTilemap : MonoBehaviour
                 }
             }
         }
-        int a = 0;
     }
-    private void _generateTileContent(Tilemap _tile)
+    [ContextMenu("GenerateXmlFile")]
+    void GenerateXmlFile()
+    {
+        _replaceInit();
+
+        int nFileIdx = 0;
+        foreach(Tilemap _mapTile in LstAssignedTitleMap)
+        {
+            if (_mapTile != null)
+            {
+                string strFileName = string.Format("{0}_{1}", GenerateFileName,nFileIdx.ToString("000"));
+                _generateTileContent(_mapTile, strFileName);
+                nFileIdx++;
+            }
+        }
+    }
+    //width="1312" height="96" tilewidth="16" tileheight="16" infinite="0" nextlayerid="2" nextobjectid="1">
+
+    private void makeXml(int width, int height, string strContent, string strFileName,int tilewidth = 16, int tileheight = 16, int infinite = 0, int nextlayerid = 2, int nextobjectid = 1)
+    {
+
+        XmlWriterSettings settings = new XmlWriterSettings();
+        settings.Indent = true;
+        settings.IndentChars = "\t";
+        if(strFileName.Length == 0)
+        {
+            strFileName = "SceneLevelBlock";
+        }
+        XmlWriter writer = XmlWriter.Create(strFileName+".tmx", settings);
+
+        //map element
+        writer.WriteStartElement("map");
+        writer.WriteStartAttribute("version");
+        writer.WriteValue("1.2");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("tiledversion");
+        writer.WriteValue("1.3.1");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("orientation");
+        writer.WriteValue("orthogonal");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("renderorder");
+        writer.WriteValue("right-down");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("compressionlevel");
+        writer.WriteValue("-1");
+        writer.WriteEndAttribute();
+
+
+        writer.WriteStartAttribute("width");
+        writer.WriteValue(width.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("height");
+        writer.WriteValue(height.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("tilewidth");
+        writer.WriteValue(tilewidth.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("tileheight");
+        writer.WriteValue(tileheight.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("infinite");
+        writer.WriteValue(infinite.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("nextlayerid");
+        writer.WriteValue(nextlayerid.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("nextobjectid");
+        writer.WriteValue(nextobjectid.ToString());
+        writer.WriteEndAttribute();
+
+        //tileset
+        writer.WriteStartElement("tileset");
+        writer.WriteStartAttribute("firstgid");
+        writer.WriteValue("1");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("source");
+        writer.WriteValue("MeowTileSet.tsx");
+        writer.WriteEndAttribute();
+
+        writer.WriteEndElement();//tileset end
+
+        //layer
+        writer.WriteStartElement("layer");
+        writer.WriteStartAttribute("id");
+        writer.WriteValue("1");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("name");
+        writer.WriteValue("Tile Layer 1");
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("width");
+        writer.WriteValue(width.ToString());
+        writer.WriteEndAttribute();
+
+        writer.WriteStartAttribute("height");
+        writer.WriteValue(height.ToString());
+        writer.WriteEndAttribute();
+
+        //< data encoding = "csv" >
+        writer.WriteStartElement("data");
+        writer.WriteStartAttribute("encoding");
+        writer.WriteValue("csv");
+        writer.WriteEndAttribute();
+        writer.WriteString(strContent);
+        writer.WriteEndElement();//data end
+
+        writer.WriteEndElement();//layer end
+
+
+        writer.WriteEndElement();//map end
+        writer.Flush();
+        writer.Close();
+
+    }
+    private void _generateTileContent(Tilemap _tile,string strGenerateFileName)
     {
         BoundsInt bounds = _tile.cellBounds;
         TileBase[] allTiles = _tile.GetTilesBlock(bounds);
@@ -116,6 +250,8 @@ public class CountTilemap : MonoBehaviour
             strNotInclude.Append(string.Format("{0}, ", str));
         }
         Debug.Log(strNotInclude.ToString());
+
+        makeXml(bounds.size.x, bounds.size.y, strBuf.ToString(), strGenerateFileName);
     }
 
     // Update is called once per frame
@@ -123,10 +259,10 @@ public class CountTilemap : MonoBehaviour
     {
         if( Input.GetKeyUp(KeyCode.Space) == true )
         {
-            if(AssignedTitleMap != null)
-            {
-                _generateTileContent(AssignedTitleMap);
-            }
+            //if(AssignedTitleMap != null)
+            //{
+            //    _generateTileContent(AssignedTitleMap, GenerateFileName);
+            //}
         }
         if (Input.GetKeyUp(KeyCode.H) == true)
         {
