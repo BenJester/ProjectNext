496b1c2614485766867c26275b9ef3dd14afa8e7
BenJester <jichu.yang1998@gmail.com>
2020-03-31 17:43:38 +0800

    房间演出

---
+++ Assets/Scripts/Animation/FadeOut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public float dur;
+    public float delay;
+    SpriteRenderer sr;
+    float a;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        a = sr.color.a;
+        StartCoroutine(DoFadeOut());
+    }
+
+    IEnumerator DoFadeOut()
+    {
+        yield return new WaitForSeconds(delay);
+        float timer = 0f;
+        while (timer < dur)
+        {
+            timer += Time.deltaTime;
+            sr.color = new Color(sr.color.r, sr.color.b, sr.color.g, (1 - timer / dur) * a);
+            yield return new WaitForEndOfFrame();
+        }
+        sr.color = new Color(sr.color.r, sr.color.b, sr.color.g, 0f);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Animation/TextFadeOut.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFadeOut : MonoBehaviour
+{
+    public float dur;
+    public float delay;
+    public float fadeInDur;
+    public float fadeInDelay;
+    float a;
+    Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+        a = text.color.a;
+        StartCoroutine(DoFadeOut());
+        StartCoroutine(DoFadeIn());
+    }
+    IEnumerator DoFadeOut()
+    {
+        
+        yield return new WaitForSeconds(delay);
+        float timer = 0f;
+        
+        while (timer < dur)
+        {
+            timer += Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.b, text.color.g, (1 - timer / dur) * a);
+            yield return new WaitForEndOfFrame();
+        }
+        text.color = new Color(text.color.r, text.color.b, text.color.g, 0f);
+    }
+    IEnumerator DoFadeIn()
+    {
+        
+        text.color = new Color(text.color.r, text.color.b, text.color.g, 0f);
+        yield return new WaitForSeconds(fadeInDelay);
+        float timer = 0f;
+        
+        while (timer < fadeInDur)
+        {
+            timer += Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.b, text.color.g, (timer / fadeInDur) * a);
+            yield return new WaitForEndOfFrame();
+        }
+        text.color = new Color(text.color.r, text.color.b, text.color.g, a);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
