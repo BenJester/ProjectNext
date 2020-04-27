4785e4b9833fc440015c7dd0f5bbd535e454deb7
yzitao <yzitao@foxmail.com>
2020-04-27 15:29:52 +0800

    exit area

---
+++ Assets/ExitArea.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitArea : MonoBehaviour
+{
+
+    public Hostage hostage;
+    public bool isTrigger = false;
+    public GameObject saveParticle;
+    public GameObject saveImage;
+    public SpriteRenderer image;
+    public Door_Lion door;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isTrigger && collision.gameObject == hostage.gameObject) {
+            isTrigger = true;
+            SaveHostage();
+
+        }
+    }
+
+    void SaveHostage() {
+
+        hostage.gameObject.SetActive(false);
+        GameObject ins = Instantiate(saveParticle, transform.position, Quaternion.identity);
+        Destroy(ins, 1f);
+        saveImage.SetActive(true);
+        image.color = Color.white;
+
+        if (door != null) door.Open();
+
+    }
+}
