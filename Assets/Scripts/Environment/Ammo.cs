5aa95e8972d17a9a61bdbe06d17cd0605db14179
BenJester <jichu.yang1998@gmail.com>
2020-04-28 15:50:53 +0800

    chaos

--- Assets/Scripts/Environment/Ammo.cs
+++ Assets/Scripts/Environment/Ammo.cs
@@ -7,19 +7,22 @@ public class Ammo : MonoBehaviour
     public int ammo;
     BoxCollider2D box;
     SpriteRenderer sr;
+    Thing thing;
     private void Start()
     {
         box = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+        thing = GetComponent<Thing>();
     }
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnCollisionEnter2D (Collision2D col)
     {
-        Hostage hostage = col.GetComponent<Hostage>();
+        Hostage hostage = col.collider.GetComponent<Hostage>();
         if (hostage != null)
         {
             hostage.ammo += ammo;
             box.enabled = false;
             sr.enabled = false;
+            thing.Die();
         }
     }
 }
