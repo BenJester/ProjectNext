703f1df4fe1e9118cfcb8c370bc40cc6afc2640e
yzitao <yzitao@foxmail.com>
2020-04-28 14:33:00 +0800

    re swap bug

---
+++ Assets/HostageBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostageBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject projectile;
+    Swap _swap;
+    Thing _thing;
+
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        _swap = PlayerControl1.Instance.GetComponent<Swap>();
+        _thing = GetComponent<Thing>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReSwap(float time) {
+        
+        StartCoroutine(StartReSwap(time));
+        _thing.SetShield(true);
+    }
+
+    IEnumerator StartReSwap(float _time) {
+        yield return new WaitForSeconds(_time);
+        _swap.col = transform.GetComponent<BoxCollider2D>();
+        _swap.DoSwap();
+        _thing.SetShield(false);
+
+
+    }
+}
