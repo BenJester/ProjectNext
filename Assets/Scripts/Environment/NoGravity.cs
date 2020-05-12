8c4130b2198529bbd706140f326149ac9974b3ac
BenJester <jichu.yang1998@gmail.com>
2020-05-12 18:07:47 +0800

    无重力战斗7-17

---
+++ Assets/Scripts/Environment/NoGravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoGravity : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            PlayerControl1.Instance.rb.gravityScale = 0f;
+            PlayerControl1.Instance.GetComponent<Thing>().isStandardGravity = false;
+            PlayerControl1.Instance.speed = 0f;
+        }
+
+    }
+}
