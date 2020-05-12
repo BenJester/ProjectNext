ad9b3c6f46bfd573d1613dadcd4c40732ebfde8d
yzitao <yzitao@foxmail.com>
2020-05-12 15:50:29 +0800

    level 6

--- Assets/Scripts/AreaManager.cs
+++ Assets/Scripts/AreaManager.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class AreaManager : MonoBehaviour {
 
 	//public int areaID;
 	public bool checkited=false;
     public GameObject particle;
+    public UnityEvent checkedAfterEvent;
 	
 
 	void Start () {
@@ -28,8 +29,10 @@ public class AreaManager : MonoBehaviour {
                 GameObject part1 = Instantiate(particle, transform.position, Quaternion.identity);
                 Destroy(part1,1.5f);
             }
-                
-			checkited=true;
+
+
+            checkedAfterEvent.Invoke();
+            checkited =true;
 			//print("enter!");
 			if(CheckPointTotalManager.instance)
 			CheckPointTotalManager.instance.SetPlayerPos (transform.position);
