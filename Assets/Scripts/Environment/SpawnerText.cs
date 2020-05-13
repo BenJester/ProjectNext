15d49bb10256b58be2a8c4a2ea8f10506ab04e7d
BenJester <jichu.yang1998@gmail.com>
2020-05-13 17:00:51 +0800

    多波敌人有ui提示

---
+++ Assets/Scripts/Environment/SpawnerText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnerText : MonoBehaviour
+{
+    public Text text;
+    public List<Mech_EnemySpawner> spawnerList;
+    public int currWave;
+    public int totalWave;
+    public Image progress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void UpdateSpawnerLeft()
+    {
+        totalWave = 0;
+        currWave = 0;
+        foreach (Mech_EnemySpawner spawner in spawnerList)
+        {
+            totalWave += spawner.maxNum;
+            currWave += spawner.count;
+        }
+        text.text = "第 " + currWave.ToString() + "/" + totalWave.ToString() + " 波";
+        progress.fillAmount = (float) currWave / (float) totalWave;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSpawnerLeft();
+    }
+}
